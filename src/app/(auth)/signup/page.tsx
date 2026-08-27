1d4dbe25f58525191e470ba815a34a79eb9a4158
n9n3r <kloudn9ner@gmail.com>
2026-08-27 09:40:01 +0100

    Revert "1.1.1"
    This reverts commit c2d4ac1700b2aafe2df23a65fc5d2e93d8fbc555.

--- src/app/(auth)/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from 'next';
-import { SignupForm } from './signup-form';
-
-export const metadata: Metadata = { title: 'Sign up' };
-
-export default function SignupPage() {
-  return <SignupForm />;
-}
